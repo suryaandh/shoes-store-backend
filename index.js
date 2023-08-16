debe605da52919dd6761ef565dc42f0b30b9b476
suryaandh <gdswiyasa@gmail.com>
2023-08-16 16:14:49 +0800

    create endpoint and middleware for authUser

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ const app = express()
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 const productRouter = require('./routes/products')
+const authRouter = require('./routes/auth')
+const userRouter = require('./routes/users')
 const port = 3000
 
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL).then(() => console.log("db connected")).catch((err) => console.log(err))
 
-
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
+
+
 app.use('/api/products', productRouter)
+app.use('/api/', authRouter)
+app.use('/api/users/', userRouter)
+
+
+
 app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT}!`))
